b28808a6c44eb1b972b8371f89b0010cc7f10961
MariaRitaHott <139187119+MariaRitaHott@users.noreply.github.com>
2024-06-24 15:51:01 -0300

    Update server.py

--- python/server.py
+++ python/server.py
@@ -4,18 +4,21 @@ import shutil
 import handleC
 import os
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 11111  # Port to listen on (non-privileged ports are > 1023)
+HOST = "127.0.0.1"  # Endereço padrão de interface de loopback (localhost)
+PORT = 11111  # Porta para escutar (portas não privilegiadas são > 1023)
 
 
 def handle_client(conn, addr):
+    """
+    Lida com a conexão do cliente. Recebe comandos, processa-os e envia as respostas apropriadas.
+    """
     print(f"Connected by {addr}")
     try:
         while True:
             # receber os dados
-            # caso seja vazia, então acabou o programa
             data = conn.recv(1024)
             if not data:
+                 # Se não houver dados, a conexão foi encerrada
                 break
 
             # decodificar os dados recebidos para utf-8
@@ -26,26 +29,30 @@ def handle_client(conn, addr):
             if (string.split(" ")[0] == "Carregar:"):
                 orig = "./arquivos/" + string.split(" ")[1]
 
+                # Criar diretório para o cliente, se não existir
                 os.makedirs("./" + str(addr[1]), exist_ok=True)
                 dest = "./" + str(addr[1]) + "/" + string.split(" ")[1]
 
+                # Copiar o arquivo para o diretório do cliente
                 shutil.copyfile(orig, dest)
 
                 handleC.run_make_commands('../arquivos')
             else:
                 for command in string.split("/"):
+                    # Executar o programa C com o comando recebido
                     output = handleC.run_c_program(command, addr[1])
                 if output:
+                    # Enviar a saída do programa de volta ao cliente
                     conn.sendall(output.encode("utf-8"))
         
-        # apagar o diretório criado anteriormente
+        # Apagar o diretório criado para o cliente
         shutil.rmtree('./' + str(addr[1]))
     except Exception as e:
         print(f"Exception occurred: {e}")
     finally:
         conn.close()
 
-
+# Configurar o servidor de socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
